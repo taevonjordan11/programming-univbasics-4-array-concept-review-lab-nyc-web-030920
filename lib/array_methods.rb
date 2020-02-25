de6adaa81ef793767c35a3f64282b587dd6e04a7
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-25 04:47:58 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,5 @@
 def find_element_index(array, value_to_find)
-array.length.times do |count|
-  if array[count] == value_to_find
-    return count
-   end
-   nil
- end
+  array.find_index(value_to_find)
 end
 
 def find_max_value(array)
