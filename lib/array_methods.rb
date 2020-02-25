6c2832bc1bde041f7f6c1cad68715d91c652a6d3
Taevon Jordan <taevonjordan1@gmail.com>
2020-02-25 05:09:02 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  x = arrray[0]
+  x = array[0]
   array.length.times { |index|
   if array[index] > x
     x = array[index]
